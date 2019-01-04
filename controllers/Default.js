063f10a751e8a47090e8fe73cb2ce672ab6ed2ae
Matt Thomas <Matt@Thomas.com>
2019-01-04 14:44:22 +0000

    phew routing working agaim

--- controllers/Default.js
+++ controllers/Default.js
@@ -18,17 +18,3 @@ module.exports.addNum1Num2GET = function addNum1Num2GET (req, res, next) {
     });
 };
 
-module.exports.addJsonNum1Num2GET = function addJsonNum1Num2GET (req, res, next) {
-  var num1 = req.swagger.params['num1'].value;
-  var num2 = req.swagger.params['num2'].value;
-  Default.addJsonNum1Num2GET(num1,num2)
-    .then(function (result) {
-
-      console.log("response is "+ result)
-      utils.writeJson(res, result);
-    })
-    .catch(function (result) {
-      console.log("an error occurred")
-      utils.writeJson(res, result);
-    });
-};
--- service/DefaultService.js
+++ service/DefaultService.js
@@ -23,14 +23,3 @@ exports.addNum1Num2GET = function(num1,num2) {
   });
 }
 
-exports.addJsonNum1Num2GET = function(num1,num2) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = num1 + num2;
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
