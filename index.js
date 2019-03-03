0c608a51a3d7a171a94a2b659ba99b5ae8ad4eea
Matt Thomas <Matt@Thomas.com>
2019-03-03 09:26:25 +0000

    fixed cors issue - now it works through swagger ui

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var serverPort = 8080;
+var cors = require('cors')
 
 // swaggerRouter configuration
 var options = {
@@ -29,19 +30,24 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   // Validate Swagger requests
   app.use(middleware.swaggerValidator());
 
+
+  app.use(cors())
+  //app.options('*',cors())
+
   // Route validated requests to appropriate controller
   app.use(middleware.swaggerRouter(options));
 
   // Serve the Swagger documents and Swagger UI
   app.use(middleware.swaggerUi());
-
-  /*
-  app.use(function (req, res, next) {
+  
+  // note in some versions of node/express, the err variable
+  // is not there - so you get the res.header is not a function error
+  app.use(function (err,req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
   });
-  */
+  
   
   // Start the server
   http.createServer(app).listen(serverPort, function () {
