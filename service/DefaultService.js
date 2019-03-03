77d8e78f27a6b2b62be49fd5e30140ef4714e8dc
Nadisha Robert <nadisharobert@Nadishas-MacBook.local>
2019-03-03 10:07:48 +0000

    boom

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -23,3 +23,17 @@ exports.addNum1Num2GET = function(num1,num2) {
   });
 }
 
+exports.multiplyNum1Num2GET = function(num1,num2) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = 
+    {
+      result:num1 * num2
+    }
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
